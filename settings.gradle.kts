da7e35cc4779f3ef737efa8d8f4b5e9ad6ac6886
ayukatawago <ayukatawago7@gmail.com>
2021-12-23 00:03:00 +0900

    Restructure code

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,11 +5,11 @@ pluginManagement {
         gradlePluginPortal()
         mavenCentral()
     }
-    
+
 }
 rootProject.name = "kotlin-multiplatform-todo"
 
-
 include(":androidApp")
+project(":androidApp").projectDir = file("client/androidApp")
 include(":shared")
-
+project(":shared").projectDir = file("client/shared")
